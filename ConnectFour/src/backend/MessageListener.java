09af28069d8a29f2a7a7aa192ac0ea4d2d63556b
FredGlozman <fredglozman@gmail.com>
2016-10-24 09:13:03 -0400

    changed exception handling and made minor formatting adjustments.

--- ConnectFour/src/backend/MessageListener.java
+++ ConnectFour/src/backend/MessageListener.java
@@ -7,46 +7,48 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MessageListener extends Thread{
-	int port_num;		//to be changed to a constant value
+public class MessageListener extends Thread {
+	int port_num; // to be changed to a constant value
 	ServerSocket listener_socket;
-	
-	public MessageListener(int port){
-		this.port_num=port;
-			try {
-				listener_socket=new ServerSocket(port_num);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+	public MessageListener(int port) {
+		this.port_num = port;
+		
+		try {
+			listener_socket = new ServerSocket(port_num);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			close();
+		}
 	}
-	
-	public void run(){
+
+	public void run() {
 		Socket clientSocket;
 		try {
-			while((clientSocket=listener_socket.accept())!=null){
-				InputStream input=clientSocket.getInputStream();
-				BufferedReader reader=new BufferedReader(new InputStreamReader(input));
-				int message=reader.read();
-				System.out.println("message that was received is: "+message);
-
+			while ((clientSocket = listener_socket.accept()) != null) {
+				InputStream input = clientSocket.getInputStream();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(input));
+				int message = reader.read();
 				
+				//TODO send message to frontend
+				System.out.println("message that was received is: " + message);
 			}
-		} catch (IOException e) {}
-		
-	}
-	
-	public void close(){
-		try {
-			listener_socket.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			throw new RuntimeException(e);
+		} finally {
+			close();
+		}
+	}
+
+	public void close() {
+		try {
+			if(listener_socket != null) {
+				listener_socket.close();
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
-	
-	
-
 }
--- ConnectFour/src/backend/MessageTransmitter.java
+++ ConnectFour/src/backend/MessageTransmitter.java
@@ -7,33 +7,32 @@ public class MessageTransmitter extends Thread {
 	int port_num;
 	int message;
 	String host_name;
-	
-	
-	
-	public MessageTransmitter(String hostName, int message, int port){
-		this.port_num=port;
-		this.message=message;
-		this.host_name=hostName;
-		
-		
+
+	public MessageTransmitter(String hostName, int message, int port) {
+		this.port_num = port;
+		this.message = message;
+		this.host_name = hostName;
 	}
-	
-	
+
 	@Override
-	public void run(){
-		try {
-				Socket transmit_socket=new Socket(host_name, port_num);
-			transmit_socket.getOutputStream().write(message);
-			System.out.println("message that was sent is: "+message);
-			transmit_socket.close();
-
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	public void run() {
+		Socket transmit_socket = null;
 		
+		try {
+			transmit_socket = new Socket(host_name, port_num);
+			transmit_socket.getOutputStream().write(message);
+			System.out.println("message that was sent is: " + message);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if(transmit_socket != null) {
+				try {
+					transmit_socket.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
-	
 
 }
